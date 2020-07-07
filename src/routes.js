24f818696495ead3eb8e68de2c76b576c3fe1af3
Rafael Framil <raframil@gmail.com>
2020-07-06 21:32:25 -0300

    merge conflict

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 
 const CrawlerController = require('./controllers/crawler-controller')
+const CVMCrawlerController = require('./controllers/crawler-cvm-controller')
 
 const router = Router()
 
 router.get('/api/companies', CrawlerController.demonstracaoResultado)
+router.get('/api/reports', CVMCrawlerController.index)
 
 module.exports = router
